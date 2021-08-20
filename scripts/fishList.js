bb3149724693d51187196ed7c07a262e1a95ffa8
Susie Stanley <susiestanley00@gmail.com>
2021-08-20 00:27:00 -0500

    first commit

---
+++ scripts/fishList.js
@@ -0,0 +1,24 @@
+// fishList renders individual fish objects as HTML
+// To Do: Import `getFish` from the data module
+
+import { getFish } from "./fishData.js";
+import { Fish } from "./Fish.js";
+
+export const FishList = () => {
+
+    
+    // Get a reference to the <section class="fishList"> element
+
+    const contentElement = document.querySelector(".fishList");
+    const allFishes = getFish();
+
+    
+    // Add to existing HTML in the content element
+
+    let fishHTMLRepresentation = "";
+    for (const oneThingFromTheSea of allFishes) {
+        fishHTMLRepresentation += Fish(oneThingFromTheSea);
+    }
+
+    contentElement.innerHTML += `${fishHTMLRepresentation}`
+}
