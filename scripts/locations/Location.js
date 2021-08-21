b0e90e2f798eac26055483cf0dff6f86561e320a
Susie Stanley <susiestanley00@gmail.com>
2021-08-21 12:33:28 -0500

    finished all files

--- scripts/locations/Location.js
+++ scripts/locations/Location.js
@@ -1,11 +1,17 @@
-// Fish function that renders individual fish objects as HTML 
+// Location function that renders individual location objects as HTML 
 
 export const Location = (locObj) => {
     return `<article class="location-card">
-                <ul>
-                    <li class="location-details">Country${locObj.country}</li>
-                    <li class="location-details">Climate${locObj.climate}</li>
-                </ul>
 
-            </article>`
+    <div><img class="location-image" src="images/${locObj.image}" />
+    </div>
+    
+    <h3 class="location-name">${locObj.name}</h3>
+    
+    <ul>
+        <li class="location-details">Country: ${locObj.country}</li>
+        <li class="location-details">Climate: ${locObj.climate}</li>
+    </ul>
+    
+    </article>`
 }
