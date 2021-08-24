8fd293ff670f8dbca3081fa7662ef3dfb02e840a
Susie <86315077+SusieCodes@users.noreply.github.com>
2021-08-23 19:46:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import { fishList } from "./fish/fishList.js";
+import { tipList } from "./tips/tipList.js";
+import { locationList } from "./locations/locationList.js";
+
+fishList ();
+tipList ();
+locationList ();
