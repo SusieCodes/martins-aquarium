849fcf75d9540db00f4a82fe3a8e345565f9574b
Susie <86315077+SusieCodes@users.noreply.github.com>
2021-08-23 19:45:33 -0500

    Delete main2.js

--- scripts/main2.js
+++
@@ -1,7 +0,0 @@
-import { fishList } from "./fish/fishList2.js";
-import { tipList } from "./tips/tipList.js";
-import { locationList } from "./locations/locationList.js";
-
-fishList ();
-tipList ();
-locationList ();
